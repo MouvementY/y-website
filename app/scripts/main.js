4c7a9e1ef67f8edc28946e433b5dc74c2041a6a7
Pierre Dulac <dulacpier@gmail.com>
2014-09-25 22:52:12 +0200

    Display only and only the logo on the page load

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,4 +7,15 @@ $(document).ready(function() {
         Boxme.draw(el);
     });
 
+    // make the logo full screen on page loading
+    setTimeout(function() {
+        var logoWrapper = document.getElementById('logo-full-screen'),
+            logo = logoWrapper.querySelector('img'),
+            header = document.querySelector('.main-head');
+
+        var wrapperHeight = window.innerHeight - header.offsetHeight;
+        logoWrapper.style.height = (wrapperHeight + header.offsetHeight) + "px";
+        logo.style.paddingTop = (wrapperHeight/2 - logo.height/2 + header.offsetHeight) + "px";
+    }, 500);
+
 });
