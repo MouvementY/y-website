03dd7befb379388b184d43788a866825a1cf95ca
Pierre Dulac <dulacpier@gmail.com>
2014-09-25 12:43:50 +0200

    Temporaly remove gulp-imagemin because it fails Heroku

--- gulpfile.js
+++ gulpfile.js
@@ -44,11 +44,11 @@ gulp.task('html', ['styles', 'scripts'], function () {
 
 gulp.task('images', function () {
     return gulp.src('app/images/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
+        // .pipe($.cache($.imagemin({
+        //     optimizationLevel: 3,
+        //     progressive: true,
+        //     interlaced: true
+        // })))
         .pipe(gulp.dest('dist/images'))
         .pipe($.size());
 });
