8f987fee409de1e484707e2816304cedbdf4754f
Pierre Dulac <dulacpier@gmail.com>
2014-11-19 00:16:59 +0100

    Use a responsive implementation for the "y" logo

--- app/scripts/scroll.js
+++ app/scripts/scroll.js
@@ -8,20 +8,11 @@ var mouvy = mouvy || {};
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
-    // make the logo full screen on page loading
-    // and scale it to fit the page height (needed for mobiles)
+    // reveal the Y :)
     setTimeout(function() {
-        var logoWrapper = document.getElementById('logo-full-screen'),
-            logo = logoWrapper.querySelector('img'),
-            header = document.querySelector('.main-head');
-
-        var wrapperHeight = window.innerHeight - header.offsetHeight;
-        logoWrapper.style.height = (wrapperHeight + header.offsetHeight) + 'px';
-        logoWrapper.style.paddingTop = (wrapperHeight/2 - logo.height/2 + header.offsetHeight) + 'px';
-
-        // reveal the logo
+        var logoWrapper = document.getElementById('logo-full-screen');
         logoWrapper.style.opacity = 1;
-    }, 500);
+    }, 300);
 
     // remove the hint scroll on mobile
     if (Modernizr.mq('only all and (max-width: 480px)')) {
