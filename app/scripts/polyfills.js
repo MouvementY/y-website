fed31064fa9c355c4c4dd8e80093b8b39c6ef450
Pierre Dulac <dulacpier@gmail.com>
2014-11-29 13:30:21 +0100

    Replace the manual bind shim by a solid open source project

--- app/scripts/polyfills.js
+++ app/scripts/polyfills.js
@@ -147,23 +147,3 @@ if (Array.prototype.forEach === undefined) {
         }
     };
 }
-
-// Function.prototype.bind
-if (Function.prototype.bind === undefined) {
-    Function.prototype.bind = function bind(scope) {
-        var
-        callback = this,
-        prepend = Array.prototype.slice.call(arguments, 1),
-        Constructor = function () {},
-        bound = function () {
-            return callback.apply(
-                this instanceof Constructor && scope ? this : scope,
-                prepend.concat(Array.prototype.slice.call(arguments, 0))
-            );
-        };
-
-        Constructor.prototype = bound.prototype = callback.prototype;
-
-        return bound;
-    };
-}
