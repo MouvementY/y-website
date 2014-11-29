1b19de70eb428fa50d3fbeb327ca1c4262bf80a3
Pierre Dulac <dulacpier@gmail.com>
2014-11-29 13:50:12 +0100

    Fix IE8 sharing !
    because the html5 data shim does create String objects
    the strict comparaison doesn't work :dancer:

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -255,12 +255,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 var triggerAction = function(e) {
                     e.preventDefault();
 
-                    if (el.dataset.social === 'twitter') {
+                    if (el.dataset.social == 'twitter') {
                         openPopup('https://twitter.com/intent/tweet', {
                             text: el.dataset.socialText,
                             url: el.dataset.socialUrl
                         });
-                    } else if (el.dataset.social === 'facebook') {
+                    } else if (el.dataset.social == 'facebook') {
                         openPopup('https://www.facebook.com/sharer/sharer.php', {
                             u: el.dataset.socialUrl
                         });
